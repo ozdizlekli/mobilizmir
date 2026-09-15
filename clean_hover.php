176973e108db0849a13719d4a6194ec39a512f01
zehra sıla özdizlekli <zehrasilaozdizlekli@gmail.com>
2026-09-15 13:38:24 +0300

    chore: remove all temporary one-off development scripts from root directory

--- clean_hover.php
+++
@@ -1,4 +0,0 @@
-<?php
-$css = file_get_contents('./wordpress/wp-content/themes/astra-child/style.css');
-$css = preg_replace('/\.magic-wrap:hover\s*\{.*?\}/s', '', $css);
-file_put_contents('./wordpress/wp-content/themes/astra-child/style.css', $css);
