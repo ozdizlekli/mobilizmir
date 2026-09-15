a3ab062ed9baeedf46bc8602d601f7de8d675843
zehra sıla özdizlekli <zehrasilaozdizlekli@gmail.com>
2026-09-15 13:44:21 +0300

    chore: optimize git tracking to only include custom theme and uploads, ignoring 10k+ WP core/plugin files to fix AI limits

--- wordpress/wp-content/plugins/safe-svg/dist/safe-svg-admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-data'), 'version' => '71294f47da4d778f1300');
--- wordpress/wp-content/plugins/safe-svg/dist/safe-svg-block-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '27379d4e5bf2f1f3a9db');
